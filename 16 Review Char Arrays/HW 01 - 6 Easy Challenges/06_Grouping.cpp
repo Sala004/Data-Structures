90bbe85d1fda59aeb8218d9336fd19e3397faaad
Salah Tamer <ma9060139@gmail.com>
2024-08-22 16:47:24 +0300

    Char Arrays

--- 16 Review Char Arrays/HW 01 - 6 Easy Challenges/06_Grouping.cpp
+++ 16 Review Char Arrays/HW 01 - 6 Easy Challenges/06_Grouping.cpp
@@ -6,14 +6,12 @@ int main() {
     string str;
     cin >> str;
 
-    string sub = "";
     int len = str.size();
     for (int i = 0; i < len; ++i) {
-        sub += str[i];
-        if (i == len - 1 || str[i] != str[i + 1]) {
-            cout << sub << " ";
-            sub = "";
+        if (i != 0 && str[i] != str[i - 1]) {
+            cout << " ";
         }
+        cout << str[i];
     }
 
     return 0;
